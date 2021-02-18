16f876697d0ad63a01954be9f476a3f02e5df73c
oomori-you <valsamina@gmail.com>
2021-02-18 22:57:24 +0900

    4-25

--- hello_app/src/main.js
+++ hello_app/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: function(h) {
+        h.length;
+        return (
+            <div>
+                <p>※以下はAppコンポーネントの表示</p>
+                <hr />
+                <App/>
+            </div>
+        );
+    }
+});
+
