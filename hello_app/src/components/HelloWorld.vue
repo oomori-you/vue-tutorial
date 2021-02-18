7eee36cc2f8f5cb7c886bba8389aaba440cba827
oomori-you <valsamina@gmail.com>
2021-02-18 21:52:39 +0900

    4-11

--- hello_app/src/components/HelloWorld.vue
+++ hello_app/src/components/HelloWorld.vue
@@ -3,7 +3,14 @@
     <h1>{{ title }}</h1>
     <pre v-on:click="clear">{{ message }}</pre>
     <hr>
-    <div><input type="text" v-on:keydown="type"></div>
+    <div>
+      <input type="text"
+             v-on:keypress="type"
+             v-on:keydown.delete="clear"
+             v-on:keydown.space="space"
+             v-on:keydown.enter="enter"
+      >
+    </div>
   </div>
 </template>
 
@@ -20,14 +27,22 @@ export default  {
   },
   methods: {
     type: function (event) {
-      this.message += event.key + ' ';
-      if (event.key === 'Escape') {
-        this.message = '';
+      if (event.key === 'Enter') {
+        return;
       }
+      this.message += event.key + ' ';
       event.target.value = '';
     },
     clear: function() {
       this.message = ''
+    },
+    space: function() {
+      this.message += '_ ';
+    },
+    enter: function (event) {
+      let res = this.message.split(' ').join('')
+      this.message = res.split('_').join(' ');
+      event.target.value = '';
     }
   }
 }
