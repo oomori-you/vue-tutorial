18ba7cada89ecf976884a36230aacdd3e821f880
oomori-you <valsamina@gmail.com>
2021-02-22 02:55:51 +0900

    6-3

--- nuxt_app/pages/axios.vue
+++ nuxt_app/pages/axios.vue
@@ -2,6 +2,11 @@
   <div class="container">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
+    <div>
+      <input type="text" v-model="msg" />
+      <button @click="doClick">Click</button>
+    </div>
+    <hr>
     <table>
       <tr>
         <th>User Id</th>
@@ -32,19 +37,29 @@ export default {
   data: function() {
     return {
       title: 'Axios',
+      message: 'axios sample.',
       msg: '',
-      message: 'axios sample.'
+      json_data: {}
     };
   },
-  asyncData: async function() {
-    let id = 1;
-    let result = await axios.get(url + id);
-    return { json_data: result.data };
-  }
+  methods: {
+    doClick: function(event) {
+      axios.get(url + this.msg).then((res) => {
+        this.message = 'get ID=' + this.msg;
+        this.json_data = res.data;
+      });
+    },
+  },
 }
 </script>
 
 <style>
+input {
+  font-size: 14pt
+}
+button {
+  font-size: 14pt
+}
 tr th {
   width: 150px;
   background-color: darkblue;
