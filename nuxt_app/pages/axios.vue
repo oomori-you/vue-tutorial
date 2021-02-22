79c23ffd6ffff5caa9ca6a2cd4433ede6cfe6298
oomori-you <valsamina@gmail.com>
2021-02-22 21:54:52 +0900

    6-11

--- nuxt_app/pages/axios.vue
+++ nuxt_app/pages/axios.vue
@@ -2,11 +2,29 @@
   <div class="container">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
-    <input v-model="find">
-    <button @click="getData">Click</button>
-    <hr>
-    <ul v-for="(data, key) in json_data" :key="key">
-      <li><strong>{{ key }}</strong><br>{{data}}</li>
+    <table>
+      <tr>
+        <th>Email</th>
+        <td><input v-model="email"></td>
+      </tr>
+      <tr>
+        <th>Name</th>
+        <td><input v-model="username"></td>
+      </tr>
+      <tr>
+        <th>Age</th>
+        <td><input type="number" v-model="age"></td>
+      </tr>
+      <tr>
+        <th>Tel</th>
+        <td><input v-model="tel"></td>
+      </tr>
+      <tr><th></th><td>
+        <button @click="addData">Click</button>
+      </td></tr>
+    </table>
+    <ul v-for="(data, key) in json_data">
+      <li><strong>{{key}}</strong><br>{{data}}</li>
     </ul>
   </div>
 </template>
@@ -14,23 +32,39 @@
 <script>
 const axios = require('axios')
 
-let url = 'https://valsamina-vue-default-rtdb.firebaseio.com/person.json?orderBy=%22age%22';
+let url = 'https://valsamina-vue-default-rtdb.firebaseio.com/person';
 
 export default {
   data: function() {
     return {
       title: 'Axios',
       message: 'axios sample.',
-      find: '',
+      email: '',
+      username: '',
+      tel: '',
+      age: 0,
       json_data: {}
     };
   },
   methods: {
+    addData: function() {
+      let add_url = url + '/' + this.email + '.json';
+      let data = {
+        'name': this.username,
+        'age': this.age,
+        'tel': this.tel
+      };
+      axios.put(add_url, data).then((re) => {
+        this.email = '';
+        this.username = '';
+        this.age = 0;
+        this.tel = '';
+        this.getData();
+      });
+    },
     getData: function() {
-      let range = this.find.split(',');
-      let age_url = url + "&startAt=" + range[0] + "&endAt=" + range[1];
-      axios.get(age_url).then((res) => {
-        this.message = 'get: ' + range[0] + ' < age < ' + range[1];
+      axios.get(url + '.json').then((res) => {
+        this.message = 'get all data.'
         this.json_data = res.data;
       }).catch((errpr) => {
         this.message = 'ERROR!';
